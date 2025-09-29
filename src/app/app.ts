6284bdd04705a619fe9da03746703b4fb2a77385
awangsuryarmdhn <111633856+awangsuryarmdhn@users.noreply.github.com>
2025-09-29 14:06:12 +0700

    ganti jadi nama

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('helloworld');
+  protected readonly title = signal('Awang 232102578');
 }
